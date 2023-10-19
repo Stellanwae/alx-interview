ac5835bea2e6db03a40efb9367b6716867d6ac87
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-10-19 17:05:30 +0300

    Create 0-stats.py

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""Script that stin but is interupted by keyboard input"""
+
+
+import sys
+
+def printstats(dic, size):
+    """ print info"""
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
+
+
+sts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+       "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
+
+try:
+    for line in sys.stdin:
+        if count != 0 and count % 10 == 0:
+            printstats(sts, size)
+
+        stlist = line.split()
+        count += 1
+
+        try:
+            size += int(stlist[-1])
+        except:
+            pass
+
+        try:
+            if stlist[-2] in sts:
+                sts[stlist[-2]] += 1
+        except:
+            pass
+    printstats(sts, size)
+
+
+except KeyboardInterrupt:
+    printstats(sts, size)
+    raise
