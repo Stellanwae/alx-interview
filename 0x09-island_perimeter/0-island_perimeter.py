5ef1002da402a6c49fed9029007634cde4c95598
Stellanwae <99267699+Stellanwae@users.noreply.github.com>
2023-11-30 22:21:23 +0300

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -6,13 +6,13 @@ Returns Island perimeter
 
 def island_perimeter(grid):
     """the function"""
-    perimeter = 0
+    total_perimeter = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                perimeter += 4
+                total_perimeter += 4
                 if i > 0 and grid[i-1][j] == 1:
-                    perimeter -= 2
+                    total_perimeter -= 2
                 if j > 0 and grid[i][j-1] == 1:
-                    perimeter -= 2
-    return perimeter
+                    total_perimeter -= 2
+    return total_perimeter
